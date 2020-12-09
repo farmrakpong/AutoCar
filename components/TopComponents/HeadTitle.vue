5b2343d50297d3f864d6a22d4284c7a5ae4046c3
farmrakpong <farmrakpong0@gmail.com>
2020-12-09 12:50:35 +0700

    yes

--- components/TopComponents/HeadTitle.vue
+++ components/TopComponents/HeadTitle.vue
@@ -1,9 +1,16 @@
 <template>
-  <div class="CardHeadTitle">
-    <h3>{{ title }}</h3>
+  <div>
+    <div class="CardHeadTitle">
+      <h3>{{ title }}</h3>
+    </div>
+
+    <div class="col-12 pl-0 pr-0 mt-2 mb-2">
+      <div class="row">
+        <div class="col-12 text-left pl-0 pr-0 searchProduct">ค้นหาสินค้า</div>
+      </div>
+    </div>
   </div>
 </template>
-
 <script>
 export default {
   props: ['title'],
@@ -11,17 +18,18 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
 @import '@/globalStyles/default.scss';
-
 .CardHeadTitle {
   padding-top: 1.5rem;
   padding-bottom: 1.5rem;
   border-bottom: 2px solid #eaeaea;
 
   @include respond-to($tablet) {
-  padding-top: 3%;
-  padding-bottom: 1%;
+    padding-top: 3%;
+    padding-bottom: 1%;
+  }
 }
+.searchProduct {
+  color: $btn-color;
 }
 </style>
--- pages/PurchaseList.vue
+++ pages/PurchaseList.vue
@@ -1,26 +1,12 @@
 <template>
   <div>
-    <HeadTitle title="รายการซื้อ" />
-    {{ $store.getters.getCount.length }}
-    <button @click="asd()">submit</button>
+    <HeadTitle title="สั่งซื้ออะไหล่สต๊อก / Stock Products" />
+    <!-- <button @click="asd()">submit</button> -->
   </div>
 </template>
 
 <script>
 export default {
   layout: 'content',
-  data() {
-    return {
-      text: {
-        title: 'jack',
-        phone: '0888888888',
-      },
-    }
-  },
-  methods: {
-    asd() {
-      this.$store.dispatch('add', this.text)
-    },
-  },
 }
 </script>
